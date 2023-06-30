c4306ccafff8cca90623da189c56b8081e92356c
unknown <cristianerazoc9.5@gmail.com>
2023-06-30 16:14:05 -0500

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+import { RouterModule, Routes} from '@angular/router';
+
+//home
+//contact
+//about
+
+//que componente va a manejar en la seleccion del menu
+const routes: Routes=[
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'contactos',
+    component: ContactComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
